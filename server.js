c86226b25d2e72e9dd6f72d9e64a45d6a457e051
Maxim Umblia <umblia34@gmail.com>
2025-04-13 23:00:57 +0300

    Деплой серверной части в репозиторий для продакшена

--- server.js
+++ server.js
@@ -1,70 +1,73 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connect from "./src/db/connect.js";
-import cookieParser from "cookie-parser";
-import fs from "node:fs";
-import errorHandler from "./src/helpers/errorhandler.js";
-import axios from "axios";
+  import express from "express";
+  import dotenv from "dotenv";
+  import cors from "cors";
+  import connect from "./src/db/connect.js";
+  import cookieParser from "cookie-parser";
+  import fs from "node:fs";
+  import errorHandler from "./src/helpers/errorhandler.js";
+  import axios from "axios";
 
-dotenv.config();
+  dotenv.config();
 
-const port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8000;
 
-const app = express();
+  const app = express();
 
-const corsOptions = {
-  origin:'https://task-tracker-client-ten.vercel.app',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', "UPDATE", "OPTIONS"],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  exposedHeaders: ['Set-Cookie']
-};
+  const corsOptions = {
+     origin: [
+      'https://task-tracker-client-ten.vercel.app',
+      'https://tasktrackerserver-7n88.onrender.com'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', "UPDATE", "OPTIONS"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Set-Cookie']
+  };
 
-//middleware  
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://task-tracker-client-ten.vercel.app');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
-  
-  next();
-});
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(errorHandler);
+  //middleware  
+  app.use(cors(corsOptions))
+  app.options('*', cors(corsOptions));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://task-tracker-client-ten.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    
+    next();
+  });
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(errorHandler);
 
-//routes
-const routeFiles = fs.readdirSync("./src/routes");
+  //routes
+  const routeFiles = fs.readdirSync("./src/routes");
 
-routeFiles.forEach((file) => {
-  import(`./src/routes/${file}`)
-    .then((route) => {
-      app.use("/api/v1", route.default);
-    })
-    .catch((err) => {
-      console.log("Failed to load route file", err);
-    });
-});
+  routeFiles.forEach((file) => {
+    import(`./src/routes/${file}`)
+      .then((route) => {
+        app.use("/api/v1", route.default);
+      })
+      .catch((err) => {
+        console.log("Failed to load route file", err);
+      });
+  });
 
-const server = async () => {
-  try {
-    await connect();
+  const server = async () => {
+    try {
+      await connect();
 
-    app.listen(port, () => {
-      console.log(`server is running on port ${port}`);
-    });
-  } catch (error) {
-    console.log("Failed to start server...", error.message);
-    process.exit(1);
-  }
-};
+      app.listen(port, () => {
+        console.log(`server is running on port ${port}`);
+      });
+    } catch (error) {
+      console.log("Failed to start server...", error.message);
+      process.exit(1);
+    }
+  };
 
-server()
+  server()
