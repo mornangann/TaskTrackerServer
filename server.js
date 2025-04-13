3edccd4ffcae5e7785604086bb66d73e8d415175
Maxim Umblia <umblia34@gmail.com>
2025-04-13 21:47:08 +0300

    Деплой серверной части в репозиторий для продакшена

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'https://task-tracker-client-ten.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
