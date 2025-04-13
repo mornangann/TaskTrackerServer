ff2b2c6bb2717b5bb07a0c9fda9e367bb9245183
Maxim Umblia <umblia34@gmail.com>
2025-04-13 23:21:14 +0300

    Деплой серверной части в репозиторий для кроссустройственного продакшена

--- src/controllers/auth/userController.js
+++ src/controllers/auth/userController.js
@@ -47,7 +47,7 @@ export const registerUser = asyncHandler(async (req, res) => {
     httpOnly: true, 
     secure: true,  
     sameSite: 'none', 
-    domain: 'https://tasktrackerserver-7n88.onrender.com', 
+    domain: 'tasktrackerserver-7n88.onrender.com',
     path: '/',      
     maxAge: 30 * 24 * 60 * 60 * 1000, 
     priority: 'high'
@@ -105,7 +105,7 @@ export const loginUser = asyncHandler(async (req, res) => {
       httpOnly: true, 
       secure: true,   
       sameSite: 'none', 
-      domain: 'https://tasktrackerserver-7n88.onrender.com',
+      domain: 'tasktrackerserver-7n88.onrender.com',
       path: '/',  
       maxAge: 30 * 24 * 60 * 60 * 1000, 
       priority: 'high' 
@@ -130,10 +130,13 @@ export const loginUser = asyncHandler(async (req, res) => {
 // logout user
 export const logoutUser = asyncHandler(async (req, res) => {
   res.clearCookie("token", {
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
-    path: "/",
+    httpOnly: true, 
+    secure: true,  
+    sameSite: 'none', 
+    domain: 'tasktrackerserver-7n88.onrender.com',
+    path: '/',      
+    maxAge: 30 * 24 * 60 * 60 * 1000, 
+    priority: 'high'
   });
 
   res.status(200).json({ message: "User logged out" });
