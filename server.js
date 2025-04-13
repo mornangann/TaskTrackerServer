6894ccec4b1dceed44594f6fe8cb3e97bb1ae6f2
Maxim Umblia <umblia34@gmail.com>
2025-04-13 22:06:25 +0300

    Деплой серверной части в репозиторий для продакшена

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const corsOptions = {
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', "UPDATE", "OPTIONS"],
-  allowedHeaders: ['Content-Type', 'Authorization'] 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Set-Cookie']
 };
 
 //middleware  
